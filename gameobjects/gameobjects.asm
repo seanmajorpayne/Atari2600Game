f98e0ca88cf27a8a2ab4a6efe3664927e7483bc8
seanmajorpayne <seanmajorpayne@gmail.com>
2021-06-07 15:39:57 +0900

    adding gameobjects

---
+++ gameobjects/gameobjects.asm
@@ -0,0 +1,186 @@
+    processor 6502
+
+    include "vcs.h"
+    include "macro.h"
+
+    seg code
+    org $F000
+
+Start:
+    CLEAN_START
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Set Game Colors
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldx #$70
+    stx COLUBK
+
+    ldx #%1111
+    stx COLUPF
+
+    ldx #$6E
+    stx COLUP0
+
+    ldx #$CC
+    stx COLUP1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Start a new frame by turning on VBLANK and VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+NextFrame:
+    lda #2
+    sta VSYNC
+    sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Generate the three lines of VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 3
+        sta WSYNC
+    REPEND
+
+    lda #0
+    sta VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Let TIA output 37 lines of VBLANK
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 37
+        sta WSYNC
+    REPEND
+
+    lda #0
+    sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Draw 192 visible scanlines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 10
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Draw Scoreboard
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+ScoreboardLoop:
+    lda NumberBitmap,Y
+    sta PF1
+    sta WSYNC
+    iny
+    cpy #10
+    bne ScoreboardLoop
+
+    ; Disable playfield
+    lda #0
+    sta PF1
+
+    ; Draw 50 empty scanlines
+    REPEAT 50
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Displays 10 scanlines for player one
+; Pulls data from an array of bytes defined at PlayerBitmap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+PlayerLoop:
+    lda PlayerBitmap,Y
+    sta GRP0
+    sta WSYNC
+    iny
+    cpy #10
+    bne PlayerLoop
+
+    lda #0
+    sta GRP0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Displays 10 scanlines for player two
+; Pulls data from an array of bytes defined at PlayerBitmap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+PlayerTwoLoop:
+    lda PlayerBitmap,Y
+    sta GRP1
+    sta WSYNC
+    iny
+    cpy #10
+    bne PlayerTwoLoop
+
+    lda #0
+    sta GRP1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Draw remaining 102 scanlines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 102
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Let the TIA output the 30 lines of overscan
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #2
+    sta VBLANK
+
+    REPEAT 30
+        sta WSYNC
+    REPEND
+
+    lda #0
+    sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Repeat Game Loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    jmp Start
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Store Sprites
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFE8
+PlayerBitmap:
+    .byte #%01111110    ;  ######
+    .byte #%11111111    ; ########
+    .byte #%10011001    ; #  ##  #
+    .byte #%11111111    ; ########
+    .byte #%11111111    ; ########
+    .byte #%11111111    ; ########
+    .byte #%10111101    ; # #### #
+    .byte #%11000011    ; ##    ##
+    .byte #%11111111    ; ########
+    .byte #%01111110    ;  ######
+
+    org $FFF2
+NumberBitmap:
+    .byte #%00001110    ; ########
+    .byte #%00001110    ; ########
+    .byte #%00000010    ;      ###
+    .byte #%00000010    ;      ###
+    .byte #%00001110    ; ########
+    .byte #%00001110    ; ########
+    .byte #%00001000    ; ###
+    .byte #%00001000    ; ###
+    .byte #%00001110    ; ########
+    .byte #%00001110    ; ########
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Complete ROM size to 4kb
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC
+    .word Start
+    .word Start
