a6d2e81502d07cac00793840d982d37c7e2c89a5
seanmajorpayne <seanmajorpayne@gmail.com>
2021-06-09 18:04:29 +0900

    updating project

--- project/project.asm
+++ project/project.asm
@@ -14,11 +14,20 @@
     seg.u Variables
     org $80
 
-P0XPos      byte
-P0YPos      byte
-EnemyXPos   byte
-EnemyYPos   byte
+P0XPos          byte
+P0YPos          byte
+EnemyXPos       byte
+EnemyYPos       byte
+P0Ptr           word
+P0ColorPtr      word
+BomberPtr       word
+BomberColorPtr  word
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define Constants
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+P0_HEIGHT = 9   
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  Start code
@@ -34,20 +43,45 @@ Start:
 ;;  Initialize BG, Field, & Player variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    lda #$84
-    sta COLUBK
-
-    lda #$C4
-    sta COLUPF
-
     lda #10
     sta P0YPos
 
     lda #60
     sta P0XPos
 
-    lda #%00000001
-    sta CTRLPF
+    lda #83
+    sta EnemyYPos
+
+    lda #54
+    sta EnemyXPos
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Player sprite & color pointers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #<P0Bitmap
+    sta P0Ptr
+
+    lda #>P0Bitmap
+    sta P0Ptr+1
+
+    lda #<P0Color
+    sta P0ColorPtr
+
+    lda #>P0Color
+    sta P0ColorPtr+1
+
+    lda #<BomberBitmap
+    sta BomberPtr
+
+    lda #>BomberBitmap
+    sta BomberPtr+1
+
+    lda #<BomberColor
+    sta BomberColorPtr
+
+    lda #>BomberColor
+    sta BomberColorPtr+1
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  VSync
@@ -68,37 +102,7 @@ VerticalSync:
     ldx #0
     stx VSYNC
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Set Horizontal Positioning for Player
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-HorizontalPosition: subroutine
-    lda P0XPos
-    and #$7F            ; Position must be positive
-
-    sta WSYNC
-    sta HMCLR
-
-    sec
-.DivideLoop:             ; Get remainder - 3 for fine position
-    sbc #15
-    bcs .DivideLoop
-
-    eor #7
-    asl
-    asl
-    asl
-    asl
-    sta HMP0            ; set fine position
-    sta RESP0           ; reset 15-step brute position
-    sta WSYNC           ; wait for scanline
-    sta HMOVE           ; apply find position offset
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Remaining 35 VBLANK lines
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-    ldx #35
+    ldx #37
 VerticalBlank:
     stx WSYNC
     dex
@@ -108,40 +112,146 @@ VerticalBlank:
     stx VBLANK
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  192 Visible Scanlines
+;;  Visible scanlines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 Scanline:
-    lda #%11110000
+    lda #$84
+    sta COLUBK
+    lda #$C2
+    sta COLUPF
+    lda #%00000001
+    sta CTRLPF
+    lda #$F0
     sta PF0
-    lda #%11111100
+    lda #$FC
     sta PF1
-    lda #%00000000
+    lda #0
     sta PF2
 
-    ldx #192
+    ldx #96
 .GameLineLoop:
-    stx WSYNC
+.InsideP0:
+    txa
+    sec
+    sbc P0YPos
+    cmp P0_HEIGHT
+    bcc .DrawP0Sprite
+    lda #0
+
+.DrawP0Sprite
+    tay
+    lda (P0Ptr),Y
+    sta WSYNC
+    sta GRP0
+    lda (P0ColorPtr),Y
+    sta COLUP0
+
+.InsideBomber:
+    txa
+    sec
+    sbc EnemyYPos
+    cmp P0_HEIGHT
+    bcc .DrawP1Sprite
+    lda #0
+
+.DrawP1Sprite
+    tay
+    lda #%0000101
+    sta NUSIZ1               ; stretch player1 sprite
+    lda (BomberPtr),Y
+    sta WSYNC
+    sta GRP1
+    lda (BomberColorPtr),Y
+    sta COLUP1
+    
     dex
     bne .GameLineLoop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Display Overscan
+;;  Overscan
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    lda #2
-    sta VBLANK
+    ldx #2
+    stx VBLANK
+
     ldx #30
 Overscan:
     stx WSYNC
     dex
     bne Overscan
 
+    ldx #0
+    stx VBLANK
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  Start new frame
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     jmp Frame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Graphics Data
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+P0Bitmap:
+    .byte #%00000000    ;Clear byte
+    .byte #%00010000    ;$0E
+    .byte #%11111110    ;$0E
+    .byte #%01111100    ;$0E
+    .byte #%00111000    ;$82
+    .byte #%00111000    ;$82
+    .byte #%00010000    ;$0E
+    .byte #%00010000    ;$0E
+    .byte #%00010000    ;$40
+
+P0TurnBitmap:
+    .byte #%00000000    ;Clear byte
+    .byte #%00010000    ;$0E
+    .byte #%01111100    ;$0E
+    .byte #%00111000    ;$0E
+    .byte #%00111000    ;$82
+    .byte #%00111000    ;$82
+    .byte #%00010000    ;$0E
+    .byte #%00010000    ;$0E
+    .byte #%00010000    ;$40
+
+BomberBitmap
+    .byte #%00000000    ;Clear byte
+    .byte #%01010100    ;$0E
+    .byte #%01010100    ;$30
+    .byte #%11111110    ;$30
+    .byte #%11111110    ;$30
+    .byte #%00111000    ;$30
+    .byte #%00111000    ;$30
+    .byte #%00010000    ;$30
+    .byte #%00010000    ;$40
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Color Data
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+P0Color:
+    .byte #$00
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$82
+    .byte #$82
+    .byte #$0E
+    .byte #$0E
+    .byte #$40
+
+BomberColor
+    .byte #$00
+    .byte #$0E
+    .byte #$30
+    .byte #$30
+    .byte #$30
+    .byte #$30
+    .byte #$30
+    .byte #$30
+    .byte #$40
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  Finish ROM with 4kb
